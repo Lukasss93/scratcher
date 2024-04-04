b4b4d19c2cde2675cff23e21c8d7f518a12117d6
Luca Patera <lucapatera@outlook.it>
2024-04-04 20:38:52 +0200

    Add .editorconfig + reformat code

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,31 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://electron-vite.github.io" target="_blank">
-      <img src="/electron-vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Electron + Vite + Vue 3" />
+    <div>
+        <a href="https://electron-vite.github.io" target="_blank">
+            <img src="/electron-vite.svg" class="logo" alt="Vite logo"/>
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
+        </a>
+    </div>
+    <HelloWorld msg="Electron + Vite + Vue 3"/>
 </template>
 
 <style scoped lang="scss">
 .logo {
-  height: 8em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+    height: 8em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
     @apply inline-block;
 
     &:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
+        filter: drop-shadow(0 0 2em #646cffaa);
     }
 
     &.vue:hover {
-      filter: drop-shadow(0 0 2em #42b883aa);
+        filter: drop-shadow(0 0 2em #42b883aa);
     }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app').$nextTick(() => {
-  // Use contextBridge
-  window.ipcRenderer.on('main-process-message', (_event, message) => {
-    console.log(message)
-  })
+    // Use contextBridge
+    window.ipcRenderer.on('main-process-message', (_event, message) => {
+        console.log(message)
+    })
 })
